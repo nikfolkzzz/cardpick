9ae6be0f4cae69009d61f3938852110b0e9261f2
Andru Nikolaev <aeanst514@yandex.ru>
2020-03-09 14:21:52 +0400

    first commit

---
+++ precache-manifest.4a1d0dc3281299a35209b9abebc1959a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da9eb4d4e2b4b03716029e2fdf0dd19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce5d6ba7f386e7859d65",
+    "url": "./static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "00ac4291f5aea3033d16",
+    "url": "./static/css/main.159c8bef.chunk.css"
+  },
+  {
+    "revision": "ce5d6ba7f386e7859d65",
+    "url": "./static/js/2.48c48da8.chunk.js"
+  },
+  {
+    "revision": "0297e416e0ae6278314551fb47a206df",
+    "url": "./static/js/2.48c48da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ac4291f5aea3033d16",
+    "url": "./static/js/main.bfc94659.chunk.js"
+  },
+  {
+    "revision": "c8b1ba469b28a45afee5",
+    "url": "./static/js/runtime-main.d04f09b7.js"
+  },
+  {
+    "revision": "858e78de41852393f1fe4ce698d526c5",
+    "url": "./static/media/giphy.858e78de.webp"
+  },
+  {
+    "revision": "e12911bc78d19ecd13df999e8bc034da",
+    "url": "./static/media/giphy.e12911bc.gif"
+  }
+]);
